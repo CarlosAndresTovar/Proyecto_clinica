9f0c859d35e4876cc79ad7ff49659cb25bae9224
Fabianch01978 <104341664+Fabianch01978@users.noreply.github.com>
2022-11-18 00:06:23 -0500

    Ajustes a los archivos de recuperar
    Ajustes a los archivos de recuperar

--- src/pagesDesktop/Recover/Recover.jsx
+++ src/pagesDesktop/Recover/Recover.jsx
@@ -1,48 +1,96 @@
 import React from "react";
 import './Recover.css';
-import Logo_Principal from './Images/Logobarra.png';
+import LogoBarra from '../Images/LogoBarra.png'
+import IconoFacebook from '../Images/facebook.png'
+import IconoLinkedin from '../Images/linkedin.png'
+import IconoTwitter from '../Images/twitter.png'
+import IconoGmail from '../Images/google.png'
+
 
 const RecoverDesktop = () => {
     return (
-        <body>
+       
+       <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Recuperar</title>
+            </head>
             <header>
-                <nav className="barra">
-                    <div className="logo-barra-superior">
-                        <img src={Logo_Principal} />
-                    </div>       
-                </nav>
-            </header>
-            <container >
-                <span className="titulo-recuperar">Recuperar la contraseña</span>
-                <p className="texto-recuperar"> Al hacer clic en Continuar recibiras un link al correo registrado, para la recuperación de la contraseña.</p>
-                
-            </container> 
-            <hr className="linea-1"></hr>   
-            <form>
-                <input className="cuadro-dialogo"></input>
-            </form>  
-            <div>
-                <button className="boton-cancelar">Cancelar</button>
-                
-            </div>
-            <div>
-                <button className="boton-enviar">Continuar</button>
-            </div>
-            <footer>
-                <nav className="barra-inferior">
-                    <p className="texto1-barra-inferior">Siguenos en nuestras redes sociales:</p>
-                    <hr className="linea-2"/>
-                    <p className="texto2-barra-inferior">Nuestras Sedes:</p>
+            <nav>
+                    <div className="container-logo">
+                        <img id="logobarra" src={LogoBarra} alt="logo" />
+                    </div>
                     
+            </nav>
+            </header>
 
-                </nav>
+            <body>
                 
+                
+            </body>
+            <footer>
+                    <div className="caja-padre-footer-seccion-1">
+                        <h6>VISITANOS EN NUESTRAS REDES SOCIALES: </h6>
+                        <div className="iconos-redes-sociales-1">
+                            <img id="icons" src={IconoFacebook} alt="iconofacebook"/>
+                        </div>
+                        <div className="iconos-redes-sociales-2">
+                            <img id="icons" src={IconoLinkedin} alt="iconofacebook"/>
+                        </div>
+                        <div className="iconos-redes-sociales-3">
+                            <img id="icons" src={IconoTwitter} alt="iconofacebook"/>
+                        </div>  
+                        <div className="iconos-redes-sociales-4">
+                            <img id="icons" src={IconoGmail} alt="iconofacebook"/>
+                        </div> 
+
+                    </div>
+
+                    <div className="caja-padre-footer-seccion-2">
+                        
+                        <h6>NUESTRAS SEDES: </h6>
+                        <div className="acerca-de">
+                                <ul>
+                                    <li>Bogota, Calle 26 #68 - 32</li>
+                                    <li>Medellin, Calle 49 #63 - 100 Laureles Estadio</li>                                    
+                                </ul>
+                        </div>
+                        <hr className="vertical-line" />
+                    </div>
+
+                    <div className="caja-padre-footer-seccion-3">
+                        <h6>LINEAS DE ATENCIÓN: </h6>
+                        <div className="acerca-de">
+                                <ul>
+                                    <li>601-9783402</li>
+                                    <li>605-9783402</li>
+                                    <li>313-489-5602</li>
+                                </ul>
+                        </div>
+                        <hr className="vertical-line" />
+                    </div>
+
+                    <div className="caja-padre-footer-seccion-4">
+                        <h6>ACERCA DE LA CLINICA: </h6>
+                            <div className="acerca-de">
+                                <ul>
+                                    <li>Servicios</li>
+                                    <li>Nosotros</li>
+                                    <li>Contactenos</li>
+                                </ul>                                
+                            </div>
+                            <hr className="vertical-line" />
+
+                    </div>                         
             </footer>
-        </body>
-                     
+       </html>
+
+
+    )
 
 
 
-       )
 }
 export default RecoverDesktop;
