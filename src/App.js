bd4af59701a8cdef016ab0d7ad5e3ce215cd1e0b
Fabianch01978 <104341664+Fabianch01978@users.noreply.github.com>
2022-11-18 11:17:34 -0500

    Inicio de Login Desktop
    Inicio de Login Desktop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import Login from './pages/Login/Login';
 import React, {useState, useEffect} from 'react';
-import RecoverDesktop from './pagesDesktop/Recover/Recover';
+import LoginDesktop from './pagesDesktop/Login/login';
 
 function App() {
 
   const [matches, setMatches] = useState(
-    window.matchMedia("(min-width: 800px)").matches
+    window.matchMedia("(min-width: 1200px)").matches
   )
 
   useEffect(() => {
     window
-    .matchMedia("(min-width: 800px)")
+    .matchMedia("(min-width: 1200px)")
     .addEventListener('change', e => setMatches( e.matches ));
   }, []);
 
   return (
     <div className="App-login">
       {!matches && <Login />}
-      {matches && <RecoverDesktop />}
+      {matches && <LoginDesktop />}
     </div>
   );
 }
