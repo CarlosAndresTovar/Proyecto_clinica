cf832d40de177e14557b78bd5f58d9cc28de4514
Fabianch01978 <104341664+Fabianch01978@users.noreply.github.com>
2022-11-20 23:53:23 -0500

    Cuadros de dialogo, checkbox y boton de enviar.
    maquetando Cuadros de dialogo, checkbox y boton de enviar.

--- src/pagesDesktop/Register/Register.jsx
+++ src/pagesDesktop/Register/Register.jsx
@@ -16,14 +16,38 @@ const RegisterDesktop = () => {
                         </div>                    
                     </nav>
                 </header>
-                <div>
-                    <span>Pagina de Registro</span>
-                </div>
-                    
-                <FooterDesktop />
-
+                <div className="container-registro">                    
+                    <div className="container-formulario-1">   
+                                                                   
+                        <form>  
+                            <span className="registrarse">Registrarse</span>
+                            <input className="cajas" type="text" placeholder="   Usuario"/>
+                            <input className="cajas" type="text" placeholder="   Contraseña"/>
+                            <input className="cajas" type="text" placeholder="   Nombres"/>
+                            <input className="cajas" type="text" placeholder="   Apellidos"/>
+                            <input className="cajas" type="text" placeholder="   Tipo de indetificacion"/><br></br>
+                            <p className="condiciones">Al dar clic en enviar, Aceptas que Clinica Imperial almacene y procese la informacion personal suministrada arriba.</p>                          
                             
-                    
+                        </form>                        
+                    </div> 
+                    <div className="linea">
+                        <hr className="ln"/>
+                    </div>
+                    <div className="container-formulario-2">
+                        <form>
+                            <span className="registrarse"></span>
+                            <input className="cajas" type="text" placeholder="   Numero de identificación"/>
+                            <input className="cajas" type="text" placeholder="   Fecha de Nacimiento"/>
+                            <input className="cajas" type="text" placeholder="   Correo"/>
+                            <input className="cajas" type="text" placeholder="   Numero Movil"/>                            
+                            <input className="checkbox" type="checkbox" />
+                            <input className="checkbox" type="checkbox" />
+                            <button type="input">Enviar</button>
+                        </form>
+                    </div>
+
+                </div>                
+                <FooterDesktop />                   
             </>
 
 
