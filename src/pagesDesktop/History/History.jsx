ab4dc9011fc44a7b3eb38b1f91ac6cdc18733bd9
Fabianch01978 <104341664+Fabianch01978@users.noreply.github.com>
2022-11-24 19:26:21 -0500

    Finalización pantalla de Historial Clínico
    Finalización pantalla de Historial Clínico

--- src/pagesDesktop/History/History.jsx
+++ src/pagesDesktop/History/History.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Services.css';
+import './History.css';
 import NavBar from '../NavBar/NavBar'
 import FooterDesktop from "../Footer/Footer";
 
@@ -7,16 +7,35 @@ const ServicesDesktop = () => {
     return(
         <>
             <NavBar />
-            <div>
-                <p>Trabajando en pantalla de servicios</p>
+            <div className="container-buttons">
+                <div className="container-buttons-internal">
+                    <div className="container-text-title">
+                        <span className="principal-title">Historial Clínico</span>
+                    </div>                     
+                    <div className="button-box">
+                        <button className="button" type="input">Evoluciones</button>
+                    </div>
+                    <div className="button-box">
+                        <button className="button" type="input">Autorizaciones Medicas</button>
+                    </div>                   
+                </div>
+                <div className="line-division">
+                    <hr className="division-line"/>
+                </div>
+                <div className="container-buttons-internal">
+                    <div className="container-text-title">
+                        <span className="principal-title"></span>
+                    </div>                     
+                    <div className="button-box">
+                        <button className="button" type="input">Historial de Medicamentos</button>
+                    </div>
+                    <div className="button-box">
+                        <button className="button" type="input">Imagenes Diagnosticas</button>
+                    </div>                   
+                </div>               
             </div>
-            <FooterDesktop />
-               
+            <FooterDesktop />              
         </>
-
-
-
     )
-
 }
 export default ServicesDesktop;
